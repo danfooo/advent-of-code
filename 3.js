9fad8dd99af73e77ef7049274839219d483875c8
Daniel Herzog <danfooo@spotify.com>
2017-12-03 15:46:04 +0100

    3.2

--- 3.js
+++ 3.js
@@ -94,5 +94,105 @@ const distance = Math.abs(at.row) + Math.abs(at.col)
 console.log(distance)
 
 // 2
+const Grid2 = class {
+  constructor() {
+    this.data = []
+    this.pointerRow = 0
+    this.pointerCol = 0
+    this.pointerDirection = 0
+    this.directions = [
+      {
+        row: 0,
+        col: 1
+      },
+      {
+        row: -1,
+        col: 0
+      },
+      {
+        row: 0,
+        col: -1
+      },
+      {
+        row: 1,
+        col: 0
+      }
+    ]
+    this.steps = -1
+    this.changeDirectionAt = 1
+    this.stepsPerDirection = 1
+  }
+  get(row, col) {
+    let rowData = this.data[row]
+    return rowData && rowData[col] ? rowData[col] : 0
+  }
+  getPosition(value) {
+    for (let rowIndex = this.pointerRow; rowIndex < this.data.length; rowIndex++) {
+      const row = this.data[rowIndex]
+      for (let colIndex = this.pointerCol; colIndex < row.length; colIndex++) {
+        if (row[colIndex] === value) {
+          return {
+            col: colIndex,
+            row: rowIndex
+          }
+        }
+      }
+    }
+  }
+  addNext(initial) {
+    let row = this.pointerRow
+    let col = this.pointerCol
+    let rowData = this.data[row] || (this.data[row] = [])
 
+    let value
+    if (initial) {
+      value = 1
+    }
+    else {
+      value = [
+        this.get(row, col - 1),
+        this.get(row, col),
+        this.get(row, col + 1),
 
+        this.get(row - 1, col - 1),
+        this.get(row - 1, col),
+        this.get(row - 1, col + 1),
+
+        this.get(row + 1, col - 1),
+        this.get(row + 1, col),
+        this.get(row + 1, col + 1)
+      ].reduce((acc, curr) => acc + curr, 0)
+    }
+
+    rowData[col] = value
+    this.lastValue = value
+
+    // Does the direction need to change?
+    this.steps++
+    if (this.steps === this.changeDirectionAt) {
+      this.pointerDirection = (this.pointerDirection + 1) % this.directions.length
+
+      this.changeDirectionAt = this.changeDirectionAt + this.stepsPerDirection
+      // After direction changes where col changes, we up stepsPerDirection
+      if (this.pointerDirection % 2) {
+        this.stepsPerDirection++
+      }
+    }
+
+    // Move
+    let dir = this.directions[this.pointerDirection]
+    this.pointerRow += dir.row
+    this.pointerCol += dir.col
+
+    return value
+  }
+}
+
+let grid2 = new Grid2
+const initial = true
+grid2.addNext(initial)
+
+// let target = 347991 // Same as in .1
+while (grid2.addNext() < target) {
+}
+console.log(grid2.lastValue)
