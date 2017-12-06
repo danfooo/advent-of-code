b4e0c325bde865685c568d4c9afaadc90aa9ad7e
Daniel Herzog <danfooo@spotify.com>
2017-12-06 18:27:38 +0100

    6.2

--- 6.js
+++ 6.js
@@ -33,3 +33,52 @@ while (!swapper.done()) {
   swapper.distribute()
 }
 console.log(swapper.swapCount)
+
+// 2
+const Swapper2 = class {
+  constructor(initialBanks) {
+    this.phase = 1
+    this.banks = [...initialBanks]
+    this.swapCount = 0
+    this.statesSeen = {}
+    this.repeatedState = null
+  }
+
+  done() {
+    if (this.phase === 1) {
+      if (this.statesSeen[this.banks.join()]) {
+        // Enter phase two: How long until we see the repeated state again?
+        this.repeatedState = this.banks.join()
+        this.phase = 2
+      }
+    }
+    else {
+      return this.repeatedState === this.banks.join()
+    }
+  }
+
+  distribute() {
+    if (this.phase === 1) {
+      this.statesSeen[this.banks.join()] = true
+    }
+
+    let value = Math.max(...this.banks)
+    let index = this.banks.indexOf(value)
+    this.banks[index] = 0
+    while (value) {
+      index = (index + 1) % this.banks.length
+      this.banks[index]++
+      value--
+    }
+    if (this.phase === 2) {
+      this.swapCount++
+    }
+  }
+}
+
+const swapper2 = new Swapper2(banks)
+
+while (!swapper2.done()) {
+  swapper2.distribute()
+}
+console.log(swapper2.swapCount)
